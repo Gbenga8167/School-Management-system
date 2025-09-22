6928433ed27872522e2af7f0fd8219b093ae8566
Gbenga8167 <gbengaayansola39@gmail.com>
2025-09-22 23:09:47 +0100

    Update clear_cache.php
    Update clear cache in public

--- public/clear_cache.php
+++ public/clear_cache.php
@@ -1,11 +1,18 @@
 <?php
+
+use Illuminate\Contracts\Console\Kernel;
+
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Kernel::class);
 
-$kernel->call('config:clear');
-$kernel->call('cache:clear');
-$kernel->call('route:clear');
+try {
+    $kernel->call('config:clear');
+    $kernel->call('cache:clear');
+    $kernel->call('route:clear');
 
-echo "✅ Laravel caches cleared!";
+    echo "✅ Laravel caches cleared successfully!";
+} catch (Exception $e) {
+    echo "❌ Error: " . $e->getMessage();
+}
