94e18322068906560316ab61ef0c5bad6115e884
Gbenga8167 <gbengaayansola39@gmail.com>
2025-09-24 23:37:00 +0100

    Update teacher.php
    Update

--- app/Models/teacher.php
+++ app/Models/teacher.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
-class Teacher extends Model
+class teacher extends Model
 {
     protected $guarded = [];
     
