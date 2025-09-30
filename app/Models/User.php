7ee2975187085d9722cd5ed0bde6e20690cd1939
Ayansola Gbenga Moses <gbengaayansola39@gmail.com>
2025-09-30 01:56:20 -0700

    student update in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\student;
+use App\Models\Student;
 use App\Models\Teacher;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
