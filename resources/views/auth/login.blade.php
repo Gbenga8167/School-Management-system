947c46ca20ffa43b26ab7db6e05a4ec3b41940e5
Ayansola Gbenga Moses <gbengaayansola39@gmail.com>
2025-09-29 22:26:46 -0700

    login changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,78 +21,75 @@
 
     </head>
 
-    <body class="auth-body-bg">
-        <div class="bg-overlay"></div>
-        <div class="wrapper-page">
-            <div class="container-fluid p-0">
-                <div class="card">
-                    <div class="card-body">
+    <body class="auth-body-bg d-flex justify-content-center align-items-center min-vh-100">
+    <div class="bg-overlay"></div>
 
-                
-                        <h1 class="text-muted text-center font-size-20"> Log In </h1>
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h1 class="text-muted text-center font-size-20">Log In</h1>
                         <div class="p-3">
-                        
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <input class="form-control  @error('user_name') is-invalid @enderror" type="text"   name="user_name" 
-                                        :value="old('user_name')" 
-                                        placeholder="Username">
+                                        <input class="form-control @error('user_name') is-invalid @enderror" 
+                                               type="text" name="user_name" 
+                                               value="{{ old('user_name') }}" 
+                                               placeholder="Username">
                                         @error('user_name')
-                                        <span class="text-danger">{{ $message }}</span>
+                                            <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>
-    
+
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <input class="form-control @error('password') is-invalid @enderror" type="password" name="password" 
-                                        :value="old('Password')" placeholder="Password">
+                                        <input class="form-control @error('password') is-invalid @enderror" 
+                                               type="password" name="password" 
+                                               placeholder="Password">
                                         @error('password')
-                                        <span class="text-danger">{{ $message }}</span>
+                                            <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>
-    
+
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                            <label class="form-label ms-1" for="customCheck1">Remember me</label>
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="customCheck1">
+                                            <label class="form-check-label" for="customCheck1">Remember me</label>
                                         </div>
                                     </div>
                                 </div>
-    
+
                                 <div class="form-group mb-3 text-center row mt-3 pt-1">
                                     <div class="col-12">
                                         <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Log In</button>
                                     </div>
                                 </div>
-    
+
                                 <div class="form-group mb-0 row mt-2">
                                     <div class="col-sm-7 mt-3">
                                         <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
                                     </div>
-                                    <div class="col-sm-5 mt-3">
+                                    <div class="col-sm-5 mt-3 text-end">
                                         <a href="auth-register.html" class="text-muted"><i class="mdi mdi-account-circle"></i> Create an account</a>
                                     </div>
                                 </div>
 
-                               
                             </form>
                         </div>
-                        <!-- end -->
-                    </div>
-                    <!-- end cardbody -->
-                </div>
-                <!-- end card -->
+                    </div><!-- end card-body -->
+                </div><!-- end card -->
             </div>
-            <!-- end container -->
         </div>
-        <!-- end -->
+    </div>
+
+
 
         <!-- JAVASCRIPT -->
         <script src="{{asset('BackendTem/assets/libs/jquery/jquery.min.js')}}"></script>
