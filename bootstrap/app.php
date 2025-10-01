91011aa7e4b8c21ebc98e3facdab18631563600a
Ayansola Gbenga Moses <gbengaayansola39@gmail.com>
2025-10-01 00:39:47 -0700

    Fectch Api for student ans subject

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,9 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
             'teachers' => TeacherMiddleWare::class,
             'students' => StudentMiddleWare::class,
         ]);
+
+        // 🔥 Add global CORS middleware
+        $middleware->global([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
 
-    
