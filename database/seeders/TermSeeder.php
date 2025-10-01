cdabc155b76cf568171c15539dbe91b90f11eaf0
Ayansola Gbenga Moses <gbengaayansola39@gmail.com>
2025-09-30 22:41:02 -0700

    TermSeeder

--- database/seeders/TermSeeder.php
+++ database/seeders/TermSeeder.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class TermSeeder extends Seeder
 {
+
     /**
+     * 
      * Run the database seeds.
      */
      public function run(): void
